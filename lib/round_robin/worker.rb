ad0a5bbd284fb711cf5f464c0e42af24c966c8c7
Jan Andersson <jan.andersson@gmail.com>
2012-03-01 19:29:43 +0100

    Added sleep in RoundRobin::Worker

--- lib/round_robin/worker.rb
+++ lib/round_robin/worker.rb
@@ -1,10 +1,18 @@
 module RoundRobin
   class Worker
     attr_accessor :shutdown
+
     def work
       begin
         RoundRobin::Job.first.invoke_job
+        sleep sleep_time
       end while not shutdown
+    end
+
+    private
+
+    def sleep_time
+      1
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'active_record'
+require 'round_robin'
 gem 'sqlite3-ruby'
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => ':memory:')
@@ -15,5 +16,11 @@ end
 
 class MyJob
   def perform(id)
+  end
+end
+
+class RoundRobin::Worker
+  def sleep_time
+    0
   end
 end
