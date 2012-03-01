c4a4ce833c3b1a5be301488b54a018873c472ea7
Jan Andersson <jan.andersson@gmail.com>
2012-03-01 19:23:46 +0100

    Implemented RoundRobin::Worker

--- lib/round_robin/worker.rb
+++ lib/round_robin/worker.rb
@@ -1,7 +1,10 @@
 module RoundRobin
   class Worker
+    attr_accessor :shutdown
     def work
-      
+      begin
+        RoundRobin::Job.first.invoke_job
+      end while not shutdown
     end
   end
 end
--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
+require 'round_robin'
 describe RoundRobin::Worker do
+  describe "#work" do
+    before do
+      @worker = RoundRobin::Worker.new
+      RoundRobin.clear
+    end
 
+    it "should perform a job" do
+      job = RoundRobin::Job.create(:handler => {:class => "MyJob", :args => [42]}.to_json)
+      @worker.should_receive(:shutdown).and_return(true)
+      RoundRobin::Job.any_instance.should_receive(:invoke_job)
+      @worker.work
+    end
+  end
 end
 
