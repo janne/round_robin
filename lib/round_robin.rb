f65d960712ca2b8310fb54434e630ca4ffa11f60
Jan Andersson <jan.andersson@gmail.com>
2012-03-01 19:00:54 +0100

    Make tests pass by adding functionality to RoundRobin.add

--- lib/round_robin.rb
+++ lib/round_robin.rb
@@ -3,7 +3,8 @@ require 'round_robin/job'
 
 module RoundRobin
   def self.add(klass, *args)
-
+    json = {:class => klass, :args => args}.to_json
+    RoundRobin::Job.create(:handler => json)
   end
 
   def self.remove(klass, *args)
