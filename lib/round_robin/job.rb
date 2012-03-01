33f5b4ba53af37064dca81278b15ec5b7ecbb78e
Kristian Hellquist <kristian.hellquist@mynewsdesk.com>
2012-03-01 20:49:39 +0100

    Add RoundRobin::Job#invoke_job

--- lib/round_robin/job.rb
+++ lib/round_robin/job.rb
@@ -1,5 +1,26 @@
+require 'json'
+
 module RoundRobin
   class Job < ActiveRecord::Base
     self.table_name = 'round_robin_jobs'
+
+    def invoke_job
+      begin
+        update_attributes(:started_at =>  Time.now, :finished_at => nil)
+        klass = Module.const_get(parsed_handler["class"])
+        args = parsed_handler["args"]
+        klass.perform(*args)
+      rescue  Exception => e
+
+      ensure
+        update_attribute(:finished_at, Time.now)
+      end
+    end
+
+
+    private
+    def parsed_handler
+      @parsed_handler ||= JSON.parse(handler)
+    end
   end
 end
--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -1,4 +1,32 @@
 require 'spec_helper'
 describe RoundRobin::Job do
-
+  describe "#invoke_job" do
+    before do
+      @job = RoundRobin::Job.new :handler => {:class => "MyJob", :args => [1]}.to_json
+    end
+    it "performs the job" do
+      MyJob.should_receive(:perform).with(1)
+      @job.invoke_job
+    end
+    it "updates finished_at" do
+      lambda{
+        @job.invoke_job
+      }.should change(@job, :finished_at).from(nil)
+    end
+    it "updates started_at" do
+      lambda{
+        @job.invoke_job
+      }.should change(@job, :started_at).from(nil)
+    end
+    it "catches exceptions and updates started_at and finished_at attributes" do
+      t1 = Time.now
+      t2 = Time.now + 1
+      Time.stub(:now).and_return(t1,t2)
+      MyJob.should_receive(:perform).and_raise(Exception)
+      @job.invoke_job
+      @job.reload
+      @job.started_at.should eql(t1)
+      @job.finished_at.should eql(t2)
+    end
+  end
 end
